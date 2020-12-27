a086ac20b1f31e8aafb314c4af2b2546fba1a15f
carlosmgc2003 <carlosmgc2003@gmail.com>
2020-12-27 12:16:41 -0300

    Actor lanza excepcion si el nombre es la cadena nula

--- model/actor.py
+++ model/actor.py
@@ -3,7 +3,10 @@ import uuid
 
 class Actor:
     def __init__(self, name: str):
-        self.name = name
+        if len(name) > 0:
+            self.name = name
+        else:
+            raise ValueError("No se puede utilizar la cadena nula")
         self.uuid = uuid.uuid4()
 
     def __str__(self):
