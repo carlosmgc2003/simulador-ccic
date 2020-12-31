90fe4bb798310946bb6d25541eddb58f12934d2b
carlosmgc2003 <carlosmgc2003@gmail.com>
2020-12-31 13:55:39 -0300

    Agregado la libreria cliente de influxDB para que el simulador pueda escribir en la base de datos

--- logger/logger.py
+++ logger/logger.py
@@ -5,6 +5,7 @@ from logger import INFLUX_DB_TOKEN, INFLUX_DB_URL
 
 
 def influxdb_logger() -> WriteApi:
+    """ Funcion que retorna una instancia de WriteApi, de acuerdo a los parametros de conexion a la BD """
     token = INFLUX_DB_TOKEN
     client = InfluxDBClient(url=INFLUX_DB_URL, token=token)
     return client.write_api(write_options=SYNCHRONOUS)
