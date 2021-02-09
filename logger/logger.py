6d363ba2c6cc618e440c0d63afc0cb5aa9303c9c
carlosmgc2003 <carlosmgc2003@gmail.com>
2021-02-09 20:31:21 -0300

    Eliminado sentencia innecesaria

--- logger/logger.py
+++ logger/logger.py
@@ -5,5 +5,4 @@ from influxdb_client.client.write_api import SYNCHRONOUS, WriteApi
 def influxdb_logger() -> WriteApi:
     """ Funcion que retorna una instancia de WriteApi, de acuerdo a los parametros de conexion a la BD """
     client = InfluxDBClient.from_config_file("config.ini")
-    bucket_api = client.buckets_api()
     return client.write_api(write_options=SYNCHRONOUS)
