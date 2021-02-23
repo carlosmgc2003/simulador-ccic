c1200079acc5d81ca1ed68eed722181a37044b0a
Carlos Maceira <cmaceira@fie.undef.edu.ar>
2021-02-23 18:39:46 -0300

    Ahora se agrega el bearer token a cada request y se usa cifrado HTTPS

--- logger/requests.py
+++ logger/requests.py
@@ -8,14 +8,37 @@ config = configparser.ConfigParser()
 config.read("config.ini")
 metrics_url = config['api-horus']['url_metricas']
 events_url = config['api-horus']['url_eventos']
-bearer_token = config['api-horus']['bearer_token']
+token_url = config['api-horus']['url_token']
+
+
+def get_bearer_token():
+    try:
+        r = requests.get("https://localhost/v1/token/list?user=admin&pass=admin", timeout=0.1, verify=False)
+        if r.status_code == 200:
+            json_response = r.json()
+            token_simulador = json_response["simulador"]
+            if len(token_simulador) == 32:
+                return token_simulador
+        r = requests.get('https://localhost/v1/token/generate?user=admin&pass=admin&device=simulador', timeout=0.1, verify=False)
+        if r.status_code == 200:
+            json_response = r.json()
+            return json_response["token"]
+    except requests.exceptions.Timeout:
+        print("Se agoto el tiempo de espera...")
+        exit(1)
+    except requests.exceptions.ConnectionError as e:
+        print("Problema con la conexion... Abortando...")
+        exit(1)
+
+
+bearer_token = get_bearer_token()
 
 headers = {"Authorization": "Bearer " + bearer_token}
 
-def api_post(url: str, endpoint: str, body: str):
 
+def api_post(url: str, endpoint: str, body: str):
     try:
-        r = requests.post(url + endpoint, data=body, timeout=0.1, headers=headers)
+        r = requests.post(url + endpoint, data=body, timeout=0.1, headers=headers, verify=False)
     except requests.exceptions.Timeout:
         print("Se agoto el tiempo de espera...")
     except requests.exceptions.ConnectionError as e:
@@ -26,7 +49,7 @@ def api_post(url: str, endpoint: str, body: str):
 
 def api_delete(url: str, endpoint: str):
     try:
-        r = requests.delete(url + endpoint, timeout=0.1, headers=headers)
+        r = requests.delete(url + endpoint, timeout=0.1, headers=headers, verify=False)
     except requests.exceptions.Timeout:
         print("Se agoto el tiempo de espera...")
     except requests.exceptions.ConnectionError:
